68628ec9b7b0297380af0e52ffd09ef99d6d14bb
黑枫叶 <454383818@qq.com>
2024-07-22 14:28:52 +0800

    update

--- internal/gcronx/start.go
+++ internal/gcronx/start.go
@@ -3,21 +3,14 @@ package gcronx
 import (
 	"fmt"
 	"time"
-
-	"github.com/gogf/gf/v2/errors/gerror"
 )
 
 func Start() error {
 	fmt.Println("gcronx-start")
 	ticker := time.NewTicker(time.Second * 3)
 	defer ticker.Stop()
-	idx := 0
 	for v := range ticker.C {
 		fmt.Println("gcronx.ticker:", v)
-		if idx >= 3 {
-			return gerror.New("ticker-idx>=3")
-		}
-		idx += 1
 	}
 	return nil
 }
