b7f4fcbb6abbf1402d0a329fc99ae35184d075d8
黑枫叶 <454383818@qq.com>
2024-07-23 14:56:35 +0800

    update

--- public/public.go
+++ public/public.go
@@ -15,11 +15,54 @@ var (
 	real_fs  = os.DirFS(core.APP_PUBLIC_DIR)
 )
 
+// FS 根据 embed 参数决定返回嵌入式文件系统还是实际文件系统。
+// 当 embed 为 true 时，返回嵌入式文件系统；否则，返回实际文件系统。
 func FS(embed bool) fs.FS {
 	if embed {
 		return embed_fs
 	} else {
 		return real_fs
+	}
+}
+
+// SubFS 根据目录名返回一个文件系统子集。
+// 它首先尝试使用现实世界中的文件系统(real_fs)来查找目录。
+// 如果目录不存在于现实世界中的文件系统中，它将尝试在嵌入的文件系统(embed_fs)中查找。
+func SubFS(dirname string) (fs.FS, error) {
+	if _, err := fs.Stat(real_fs, dirname); err != nil {
+		return fs.Sub(real_fs, dirname)
+	} else {
+		return fs.Sub(embed_fs, dirname)
+	}
+}
+
+// ReadFile 根据文件路径读取文件内容。
+// 它首先尝试从实际文件系统中读取文件。如果失败了，它会尝试从嵌入的文件系统中读取。
+// 这个函数的存在是为了提供一种从两种不同来源读取文件内容的灵活方式。
+func ReadFile(fpath string) ([]byte, error) {
+	if content, err := fs.ReadFile(real_fs, fpath); err == nil {
+		return content, err
+	} else {
+		return fs.ReadFile(embed_fs, fpath)
+	}
+}
+
+// ReadDir 读取指定目录下的文件和子目录信息。
+// 它首先尝试从现实文件系统(real_fs)中读取目录内容，如果失败，则尝试从嵌入文件系统(embed_fs)中读取。
+// 这种设计允许在不同的文件系统之间灵活切换，例如在本地开发和部署时使用不同的文件系统。
+func ReadDir(dirpath string) ([]fs.DirEntry, error) {
+	if dirs, err := fs.ReadDir(real_fs, dirpath); err == nil {
+		return dirs, err
+	} else {
+		return fs.ReadDir(embed_fs, dirpath)
+	}
+}
+
+func Stat(fpath string) (fs.FileInfo, error) {
+	if info, err := fs.Stat(real_fs, fpath); err == nil {
+		return info, err
+	} else {
+		return fs.Stat(embed_fs, fpath)
 	}
 }
 
